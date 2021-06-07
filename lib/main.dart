7eaac38adffcb7d540e0cc1e8e942972b32231f0
fortinjoffrey <fortinjoffrey@gmail.com>
2021-06-07 19:02:31 +0200

    Add exercise-solution

--- lib/main.dart
+++ lib/main.dart
@@ -10,55 +10,114 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _HomeState extends State<Home> {
+  int _redFlexValue = 1;
+  int _blueFlexValue = 2;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Exercice'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          SizedBox(
+            height: 100,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: _blueFlexValue,
+                  child: Container(
+                    color: Colors.blue,
+                    child: const Center(child: Text('Blue')),
+                  ),
+                ),
+                Expanded(
+                  flex: _redFlexValue,
+                  child: Container(
+                    color: Colors.red,
+                    child: const Center(child: Text('Red')),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Container(
+            color: Colors.green,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                  onPressed: () {
+                    setState(() => _blueFlexValue++);
+                  },
+                  child: const Text('Expanded blue flex'),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                  ),
+                  onPressed: () {
+                    setState(() => _redFlexValue++);
+                  },
+                  child: const Text('Expanded red flex'),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 15,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('My index is: $index'),
+                  tileColor: Colors.primaries[index % Colors.primaries.length],
+                );
+              },
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            child: Center(
+              child: TextButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.black),
+                ),
+                onPressed: () {
+                  setState(() {
+                    _redFlexValue = 1;
+                    _blueFlexValue = 1;
+                  });
+                },
+                child: const Text('Reset flex numbers red flex'),
+              ),
+            ),
+          )
+          // Expanded(
+          //   child: Center(child: Text('Helo')),
+          // )
+        ],
       ),
     );
   }
