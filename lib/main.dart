0c633ab76f6d3bba6dc066f9ce164b5abdcb2544
fortinjoffrey <fortinjoffrey@gmail.com>
2021-08-20 19:32:52 +0200

    Add progress percentage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
 
@@ -24,8 +25,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final streamController = StreamController<double>();
+
+  @override
+  void dispose() {
+    streamController.close();
+    super.dispose();
+  }
 
   Future<void> _saveFileToDownloadFolder({
     required BuildContext context,
@@ -55,32 +69,52 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          child: Text('Download flutter dash'),
-          onPressed: () async {
-            final remoteSource = RemoteSource();
-
-            try {
-              // (step 1): check if we have permission to storage
-              if (!await Permission.storage.isGranted) {
-                final status = await Permission.storage.request();
-
-                if (status != PermissionStatus.granted) {
-                  return;
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StreamBuilder<double>(
+              stream: streamController.stream,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final percentage = (snapshot.data! * 100).toInt();
+                  return Text(
+                    '$percentage %',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 48),
+                  );
                 }
-              }
+                return SizedBox.shrink();
+              },
+            ),
+            ElevatedButton(
+              child: Text('Download flutter dash'),
+              onPressed: () async {
+                final remoteSource = RemoteSource();
 
-              // (step 2): once the permission is granted, download the file
-              final fileBytes = await remoteSource.downloadFromUrl(
-                  'https://flutter.dev/assets/dash/early-dash-sketches2-a38190f3c27f9309101e711143e4a67af04757d7b9f29b00e9ae8cb2cb841d1a.jpg');
+                try {
+                  // (step 1): check if we have permission to storage
+                  if (!await Permission.storage.isGranted) {
+                    final status = await Permission.storage.request();
 
-              // (step 3): save the file into the download folder
-              await _saveFileToDownloadFolder(context: context, fileBytes: fileBytes, filename: 'dash.jpg');
-            } catch (e) {
-              print(e);
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('An error occured')));
-            }
-          },
+                    if (status != PermissionStatus.granted) {
+                      return;
+                    }
+                  }
+
+                  // (step 2): once the permission is granted, download the file
+                  final fileBytes = await remoteSource.downloadFromUrl(
+                    'https://flutter.dev/assets/dash/early-dash-sketches2-a38190f3c27f9309101e711143e4a67af04757d7b9f29b00e9ae8cb2cb841d1a.jpg',
+                    streamController,
+                  );
+
+                  // (step 3): save the file into the download folder
+                  await _saveFileToDownloadFolder(context: context, fileBytes: fileBytes, filename: 'dash.jpg');
+                } catch (e) {
+                  print(e);
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('An error occured')));
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
--- lib/remote_source.dart
+++ lib/remote_source.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:dio/dio.dart';
@@ -5,15 +7,21 @@ import 'package:dio/dio.dart';
 class RemoteSource {
   final client = Dio();
 
-  Future<Uint8List> downloadFromUrl(String url) async {
+  Future<Uint8List> downloadFromUrl(String url, StreamController<double> progressStreamController) async {
     try {
       final result = await client.get<Uint8List>(
         url,
         options: Options(
           responseType: ResponseType.bytes,
         ),
+        onReceiveProgress: (count, total) {
+          print(count);
+          print(total);
+          final percentage = count / total;
+          print('$percentage');
+          progressStreamController.add(min(percentage, 1.0));
+        },
       );
-
       return result.data!;
     } catch (e) {
       print(e);
