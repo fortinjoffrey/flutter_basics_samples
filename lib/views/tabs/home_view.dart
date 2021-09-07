b573cd46a07c493df5bc5d597feeff2ac4b74d41
fortinjoffrey <fortinjoffrey@gmail.com>
2021-09-07 12:23:40 +0200

    Navigate to tab from a button and push a view

--- lib/views/tabs/home_view.dart
+++ lib/views/tabs/home_view.dart
@@ -38,6 +38,35 @@ class HomeView extends StatelessWidget {
               },
               child: Text('Navigate to placeholder view in root router'),
             ),
+            ElevatedButton(
+              onPressed: () {
+                AutoTabsRouter.of(context).setActiveIndex(1);
+              },
+              child: Text('Go to second tab'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                AutoTabsRouter.of(context).setActiveIndex(1);
+
+                // This delay is absolutely important for two reasons:
+                // 1. in autoroute 2.3.0, on first launch StackRouters are lazy loaded
+                //    meaning that trying to get the Profile stack router will result in null
+                //    in the first launch. Once it is init then the router will never be null.
+                //    setActiveIndex will trigger the tab router init. Since setActiveIndex is sync
+                //    we have to wait a little for the init to be done
+                // 2. This might be convenient for the user since he sees what the app is actually doing
+                //    It navigates itself and the user is aware of what's going on
+                // NB: 500ms is totally arbitrary
+                await Future<void>.delayed(const Duration(milliseconds: 500));
+                final router = AutoTabsRouter.of(context).innerRouterOf<StackRouter>(ProfileTab.name);
+
+                if (router != null) {
+                  router.popUntilRoot();
+                  router.push(ColorfulRoute());
+                }
+              },
+              child: Text('Go to profile and push red view'),
+            ),
           ],
         ),
       ),
