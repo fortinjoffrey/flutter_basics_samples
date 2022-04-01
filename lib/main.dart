6f4049c01d0150324762a08950830b03b731c365
fortinjoffrey <fortinjoffrey@gmail.com>
2022-04-01 15:42:19 +0200

    Add scroll to widget in a scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: true,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Scroll to widget'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final ScrollController _scrollController = ScrollController();
+  final blueKey = GlobalKey();
+  final scrollKey = GlobalKey();
+  final appBarKey = GlobalKey();
+  final greenKey = GlobalKey();
+  final yellowKey = GlobalKey();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      RenderBox box = blueKey.currentContext!.findRenderObject() as RenderBox;
+      Offset position = box.localToGlobal(Offset.zero); //this is global position
+      double y = position.dy;
+      print('Blue container y value = $y');
     });
   }
 
@@ -39,27 +52,108 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        key: appBarKey,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: SingleChildScrollView(
+        key: scrollKey,
+        controller: _scrollController,
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 300,
+                color: Colors.red,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _scrollController.animateTo(
+                    _scrollController.position.maxScrollExtent,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                child: const Text('Press to scroll to bottom'),
+              ),
+              Container(
+                key: blueKey,
+                height: 300,
+                color: Colors.blue,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  scrollToWidget(yellowKey, _scrollController);
+                },
+                child: const Text('Press to scroll to yellow'),
+              ),
+              Container(
+                key: greenKey,
+                height: 300,
+                color: Colors.green,
+              ),
+              Container(
+                height: 300,
+                color: Colors.cyan,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _scrollController.animateTo(
+                    _scrollController.position.minScrollExtent,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                child: const Text('Press to scroll to top'),
+              ),
+              Container(
+                key: yellowKey,
+                height: 300,
+                color: Colors.yellow,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  scrollToWidget(blueKey, _scrollController);
+                },
+                child: const Text('Press to scroll to blue'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  scrollToWidget(greenKey, _scrollController);
+                },
+                child: const Text('Press to scroll to green'),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+    );
+  }
+
+  double? getAbsoluteWidgetYPosition(GlobalKey key) {
+    final RenderBox? box = key.currentContext?.findRenderObject() as RenderBox?;
+    final Offset? position = box?.localToGlobal(Offset.zero); //this is global position
+    return position?.dy;
+  }
+
+  void scrollToWidget(
+    GlobalKey key,
+    ScrollController scrollController, {
+    Duration duration = const Duration(milliseconds: 500),
+  }) {
+    final widgetY = getAbsoluteWidgetYPosition(key);
+    final appBarHeight = appBarKey.currentContext?.size?.height;
+
+    if (widgetY == null) return;
+    if (appBarHeight == null) return;
+
+    final scrollY = _scrollController.offset;
+    final pixelsToScroll = scrollY + widgetY - appBarHeight;
+
+    _scrollController.animateTo(
+      pixelsToScroll,
+      duration: duration,
+      curve: Curves.easeInOut,
     );
   }
 }
