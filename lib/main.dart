510e58e4f9b4925c331fa73209419aa916ec3782
fortinjoffrey <fortinjoffrey@gmail.com>
2022-05-25 11:53:28 +0200

    Use some date formats

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,48 +18,63 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+                      final DateTime now = DateTime.now();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Date formatters'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Builder(
+                builder: (context) {
+                  final now = DateTime.now();
+                  return Text('DateTime.now() = $now');
+                },
+              ),
+              const SizedBox(height: 16),
+              Builder(
+                builder: (context) {
+                  // final DateTime now = DateTime.now().subtract(const Duration(days: 50));
+                  final DateFormat formatter = DateFormat('EEEE hh:mm');
+                  final String formatted = formatter.format(now);
+                  print(formatted);
+                  return Text('DateFormat(\'EEEE hh:mm\') = $formatted');
+                },
+              ),
+              const SizedBox(height: 16),
+              Builder(
+                builder: (context) {
+                  final DateFormat formatter = DateFormat.E();
+                  final String formatted = formatter.format(now);
+                  print(formatted);
+                  return Text('DateFormat.E() = $formatted');
+                },
+              ),
+              const SizedBox(height: 16),
+              Builder(
+                builder: (context) {
+                  final DateFormat formatter = DateFormat.MMMMEEEEd();
+                  final String formatted = formatter.format(now);
+                  print(formatted);
+                  return Text('DateFormat.MMMMEEEEd() = $formatted');
+                },
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
