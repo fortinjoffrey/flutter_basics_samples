be6fcc410c9365a30ea3abc264524d9cc245d2e5
fortinjoffrey <fortinjoffrey@gmail.com>
2022-05-26 21:54:20 +0200

    Share text, link, image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:http/http.dart' as http;
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,48 +24,64 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Share demo'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            ElevatedButton(
+              onPressed: () {
+                Share.share('Ipsum eiusmod qui officia nostrud pariatur.');
+              },
+              child: const Text('Share text'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                const String url = 'https://www.youtube.com/';
+                Share.share('Check out this link: $url');
+              },
+              child: const Text('Share text + link'),
+            ),
+            const SizedBox(height: 16),
+            Image.network(
+              'https://docs.flutter.dev/assets/images/dash/early-dash-sketches3.jpg',
+              width: 200,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                final String url = 'https://docs.flutter.dev/assets/images/dash/early-dash-sketches3.jpg';
+                final Uri uri = Uri.parse(url);
+                final http.Response response = await http.get(uri);
+
+                if (response.statusCode != 200) {
+                  return;
+                }
+
+                final Uint8List bytes = response.bodyBytes;
+
+                final Directory tempDir = await getTemporaryDirectory();
+                final String uuid = Uuid().v4();
+                final String path = '${tempDir.path}/$uuid.jpg';
+                File(path).writeAsBytesSync(bytes);
+
+                await Share.shareFiles([path]);
+              },
+              child: const Text('Share images'),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
