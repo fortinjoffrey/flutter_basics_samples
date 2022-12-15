30e38277fa14e00ec17846758fd644be4813cf10
fortinjoffrey <fortinjoffrey@gmail.com>
2022-12-15 14:30:32 +0100

    tmp

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:basics_samples/models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
@@ -38,39 +41,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Users list'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(child: _UsersList()),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          // FirebaseFirestore.instance.collection('users');
+          usersRef.add(User(id: id, name: name, age: age, email: email))
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
+  }
+}
+
+class _UsersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FirestoreBuilder<UserQuerySnapshot>(
+        ref: usersRef,
+        builder: (context, AsyncSnapshot<UserQuerySnapshot> snapshot, Widget? child) {
+          if (snapshot.hasError) return const Text('Something went wrong!');
+          if (!snapshot.hasData) return const Text('Loading users...');
+
+          // Access the QuerySnapshot
+          UserQuerySnapshot querySnapshot = snapshot.requireData;
+
+          return ListView.builder(
+            itemCount: querySnapshot.docs.length,
+            itemBuilder: (context, index) {
+              // Access the User instance
+              User user = querySnapshot.docs[index].data;
+
+              return Text('User name: ${user.name}, age ${user.age}');
+            },
+          );
+        });
   }
 }
---
+++ lib/models/user.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+// This doesn't exist yet...! See "Next Steps"
+part 'user.g.dart';
+
+/// A custom JsonSerializable annotation that supports decoding objects such
+/// as Timestamps and DateTimes.
+/// This variable can be reused between different models
+const firestoreSerializable = JsonSerializable(
+  converters: firestoreJsonConverters,
+  // The following values could alternatively be set inside your `build.yaml`
+  explicitToJson: true,
+  createFieldMap: true,
+);
+
+@firestoreSerializable
+class User {
+  User({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.email,
+  });
+
+  @Id()
+  final String id;
+
+  final String name;
+  final int age;
+  final String email;
+}
+
+@Collection<User>('users')
+final usersRef = UserCollectionReference();
