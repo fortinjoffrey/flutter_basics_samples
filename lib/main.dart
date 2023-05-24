1a79c5d79f18700ec9523d2e726865a1c5fc8b39
fortinjoffrey <fortinjoffrey@gmail.com>
2023-05-24 02:17:53 +0200

    Add fields with maxLength and error when chars limit exceeded

--- lib/main.dart
+++ lib/main.dart
@@ -27,13 +27,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final int _field1CharactersLimit = 20;
+  final int _field2CharactersLimit = 50;
+  String _field1Description = '';
+  String _field2Description = '';
+  bool _isField2Valid = true;
+  final GlobalKey<FormFieldState> _formFieldkey = GlobalKey<FormFieldState>();
 
   @override
   Widget build(BuildContext context) {
@@ -45,21 +44,105 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  const Text('Field with maxLength that block typing'),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    maxLength: _field1CharactersLimit,
+                    onChanged: (text) {
+                      setState(() {
+                        _field1Description = text;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      counter: Text(
+                        '${_field1Description.characters.length} / $_field1CharactersLimit',
+                      ),
+                      hintText: 'Description',
+                      fillColor: Colors.grey[300],
+                      filled: true,
+                      focusedBorder: FocusedOutlineInputBorder(),
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                        gapPadding: 0,
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  const Text('Field with limit that invalidates field'),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    key: _formFieldkey,
+                    validator: (text) {
+                      if (text == null || text.isEmpty) return null;
+
+                      if (text.characters.length > _field2CharactersLimit) {
+                        return 'Characters limit exceeded';
+                      }
+                      return null;
+                    },
+                    onChanged: (text) {
+                      if (_formFieldkey.currentState == null) return;
+                      final isValid = _formFieldkey.currentState!.validate();
+
+                      setState(() {
+                        _isField2Valid = isValid;
+                        _field2Description = text;
+                      });
+                    },
+                    maxLines: 3,
+                    decoration: InputDecoration(
+                      counter: Text(
+                        '${_field2Description.characters.length} / $_field2CharactersLimit',
+                        style: TextStyle(color: !_isField2Valid ? Colors.red : null),
+                      ),
+                      hintText: 'Description',
+                      fillColor: Colors.grey[300],
+                      filled: true,
+                      focusedErrorBorder: ErrorOutlineInputBorder(),
+                      errorBorder: ErrorOutlineInputBorder(),
+                      focusedBorder: FocusedOutlineInputBorder(),
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(width: 0, style: BorderStyle.none),
+                        gapPadding: 0,
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
+}
+
+class ErrorOutlineInputBorder extends OutlineInputBorder {
+  ErrorOutlineInputBorder()
+      : super(
+          borderSide: BorderSide(width: 1, style: BorderStyle.solid, color: Colors.red),
+          gapPadding: 0,
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+        );
+}
+
+class FocusedOutlineInputBorder extends OutlineInputBorder {
+  FocusedOutlineInputBorder()
+      : super(
+          borderSide: BorderSide(width: 1, style: BorderStyle.solid, color: Colors.blue),
+          gapPadding: 0,
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+        );
 }
