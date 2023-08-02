f471a2bf8edb86104fef40fde56c568938828789
fortinjoffrey <fortinjoffrey@gmail.com>
2023-08-02 18:51:16 +0200

    Show TextField above keyboard

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Bottom TextField above Keyboard'),
     );
   }
 }
@@ -27,38 +27,147 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final bottomColumnFieldKey = GlobalKey();
+  bool resizeImageOnKeyboardAppears = true;
+  double? initExpandedHeight;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    print('build');
+    print(MediaQuery.of(context).viewInsets.bottom);
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(
+          title: Align(child: Text(widget.title)),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        body: Container(
+          color: Colors.black,
+          child: Column(
+            children: <Widget>[
+              if (resizeImageOnKeyboardAppears)
+                Expanded(
+                    child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage('https://pbs.twimg.com/media/FKNlhKZUcAEd7FY?format=jpg&name=4096x4096'),
+                        fit: BoxFit.fitHeight),
+                    color: Colors.green,
+                  ),
+                  child: Center(
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          bottom: 20,
+                          left: 20,
+                          child: Container(
+                            height: 10,
+                            width: 10,
+                            color: Colors.red,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 30,
+                          left: 30,
+                          child: Container(
+                            height: 10,
+                            width: 10,
+                            color: Colors.red,
+                          ),
+                        ),
+                        Align(alignment: Alignment.topCenter, child: Text('foo')),
+                        Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'STACK',
+                              style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red,
+                                  ),
+                            )),
+                        Align(alignment: Alignment.bottomCenter, child: Text('bar')),
+                      ],
+                    ),
+                  ),
+                ))
+              else
+                Expanded(
+                  child: LayoutBuilder(builder: (context, constraints) {
+                    initExpandedHeight ??= constraints.maxHeight;
+                    return OverflowBox(
+                      alignment: Alignment.topCenter,
+                      maxHeight: double.infinity,
+                      minHeight: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image:
+                                  NetworkImage('https://pbs.twimg.com/media/FKNlhKZUcAEd7FY?format=jpg&name=4096x4096'),
+                              fit: BoxFit.fitHeight),
+                          color: Colors.green,
+                        ),
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(maxHeight: initExpandedHeight!),
+                          // height: initExpandedHeight,
+                          child: Center(
+                              child: Text(
+                            'OverflowBox',
+                            style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red,
+                                ),
+                          )),
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                color: Colors.white,
+                padding: const EdgeInsets.all(8.0),
+                child: SafeArea(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        scrollPadding: EdgeInsets.only(bottom: 100),
+                        maxLines: null,
+                        decoration: InputDecoration(
+                          hintText: 'Your name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(
+                              style: BorderStyle.solid,
+                              color: Colors.blue,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                      AnimatedContainer(
+                        height: MediaQuery.of(context).viewInsets.bottom == 0 ? 50 : 0,
+                        color: Colors.yellow,
+                        duration: const Duration(milliseconds: 100),
+                        child: Center(
+                          child: const Text('SECTION TO REMOVE ON KEYBOARD APPEARS'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
